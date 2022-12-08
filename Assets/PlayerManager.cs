21dae11081a91111355c66a1c8ac8c4eefd5709b
SonTonyD <sontony.dinh@gmail.com>
2022-12-08 16:03:21 +0100

    comment and code refactoring

--- Assets/PlayerManager.cs
+++ Assets/PlayerManager.cs
@@ -22,25 +22,28 @@ public class PlayerManager : MonoBehaviour
 
     private void Start()
     {
-        today = (int)System.DateTime.Now.Day;
-        Debug.Log(previousDay);
-        Debug.Log(today);
-
-        playerId = 23;
-        tasks = new ArrayList(5);
-
         progressPopup = progressPopupGameObject.GetComponent<ProgressPopUp>();
 
-        progressPopup.setWaterCondition("GOOD");
-        progressPopup.setLitterCondition("GOOD");
-        progressPopup.setAirCondition("GOOD");
-        if (today != previousDay)
+        //Get the day of today
+        today = (int)System.DateTime.Now.Day;
+
+        //Initialize the array of tasks
+        tasks = new ArrayList(5);
+
+        //Fetch Player ID From database
+        FetchPlayerID();
+
+        //Fetch Tasks of the player from database
+        //...code...
+
+        //Fetch World condition from World Design Component
+        FetchWorldCondition();
+
+        //Generate 5 daily tasks if it is a new day
+        if (isNewConnection())
         {
-            previousDay = today;
             Generate5DailyTask();
         }
-
-        //Get WorldDesign class
     }
     private void Update()
     {
@@ -49,39 +52,11 @@ public class PlayerManager : MonoBehaviour
     }
 
 
-    #region Class Methods and Constructor
+    #region IPlayerManager
+
     public PlayerManager(int playerId)
     {
         this.playerId = playerId;
-    }
-
-    public ArrayList Generate5DailyTask()
-    {
-        string[] availableTask = new string[] { "RubbishMinigame", "BrushingTeethMinigame", "SwitchLightsMinigame", "ShoppingMinigame", "TransportMinigame" };
-        int NUMBER_OF_TASK = 5;
-        for (int i = 0; i < NUMBER_OF_TASK; i++)
-        {
-            float randomNumber = Random.Range(0, 4);
-            Task currentTask = Task.CreateInstance(playerId, Random.Range(0, 1000), availableTask[(int)randomNumber], "daily", "not done");
-            this.tasks.Add(currentTask);
-            progressPopup.setTask(i, currentTask);
-        }
-        return this.tasks;
-    }
-
-    public int ComputeScore()
-    {
-        foreach (Task task in this.tasks)
-        {
-            if (task.getStatus() == "done")
-            {
-                this.playerScore += 20;
-            } else if (task.getStatus() == "failed")
-            {
-                this.playerScore -= 10;
-            }
-        }
-        return this.playerScore;
     }
 
     public void UpdateTaskStatus(int taskId, string status)
@@ -98,7 +73,71 @@ public class PlayerManager : MonoBehaviour
 
     public void DisplayPopUp(Task[] dailyTasks)
     {
-        //Call popup function and display the tasks of dailyTasks
+        //TODO Call popup function and display the tasks of dailyTasks
+        //...code...
+    }
+    #endregion
+
+    #region Private Methods
+    //compare the day number of today with the day number of the last connection stored in the database
+    private bool isNewConnection()
+    {
+        //TODO
+        //...code...
+        return true;
+    }
+
+    private ArrayList Generate5DailyTask()
+    {
+        string[] availableTask = new string[] { "RubbishMinigame", "BrushingTeethMinigame", "SwitchLightsMinigame", "ShoppingMinigame", "TransportMinigame" };
+        int NUMBER_OF_TASK = 5;
+        for (int i = 0; i < NUMBER_OF_TASK; i++)
+        {
+            float randomNumber = Random.Range(0, 4);
+            Task currentTask = Task.CreateInstance(playerId, Random.Range(0, 1000), availableTask[(int)randomNumber], "daily", "not done");
+            this.tasks.Add(currentTask);
+            progressPopup.setTask(i, currentTask);
+        }
+        return this.tasks;
+    }
+
+    private int ComputeScore()
+    {
+        foreach (Task task in this.tasks)
+        {
+            if (task.getStatus() == "done")
+            {
+                this.playerScore += 20;
+            } else if (task.getStatus() == "failed")
+            {
+                this.playerScore -= 10;
+            }
+        }
+        return this.playerScore;
+    }
+
+    private ArrayList FetchTask()
+    {
+        //TODO Fetch tasks of the player from database
+        //...code...
+        return this.tasks;
+    }
+
+    private void FetchWorldCondition()
+    {
+        //TODO : Get world condition from WorldDesign
+        //...code...
+        progressPopup.setWaterCondition("GOOD");
+        progressPopup.setLitterCondition("GOOD");
+        progressPopup.setAirCondition("GOOD");
+    }
+
+
+    private void FetchPlayerID()
+    {
+        //TODO : Fetch Player ID From Database after the registration of the player
+        //...code...
+        this.playerId = 23;
     }
 
     #endregion
